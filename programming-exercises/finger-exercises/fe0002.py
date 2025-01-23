33520e7ce109a66a9f76eab364cef25e8ab5dc8b
ilikkako <101609682+ilikkako@users.noreply.github.com>
2025-01-23 14:38:48 +0200

    Add files via upload

---
+++ programming-exercises/finger-exercises/fe0002.py
@@ -0,0 +1,9 @@
+x = input("Write a number: ")
+x = int(x)
+
+if x < 0:
+    print('The number is negative')
+elif x == 0:
+    print('The number is zero')
+else:
+    print('The number is positive')
