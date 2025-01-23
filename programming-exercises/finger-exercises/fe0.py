be55915a94a9f2fbd466adf4e5f987ad66dfd96d
ilikkako <101609682+ilikkako@users.noreply.github.com>
2025-01-23 13:47:31 +0200

    Add files via upload

---
+++ programming-exercises/finger-exercises/fe0.py
@@ -0,0 +1,4 @@
+birthday = input("What is your birthday? (DD/MM/YYYY)\n")
+print(f'You were born in the year {birthday[6:10]}')
+# The first value in slicing is the first character it includes in the slice
+# and the second value is the first character it doesn't include in the slice
---
+++ programming-exercises/problem-sets/ps0.py
@@ -0,0 +1,12 @@
+# Problem Set 0
+# Name: Ilkka Kotajärvi
+# Collaborators: nobody
+# Time spent: 5 minutes
+
+
+import numpy
+
+x = 5
+y = 8
+z = x + y
+a = numpy.log2(z)
