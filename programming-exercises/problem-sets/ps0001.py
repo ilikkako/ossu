b3263fc2ab79bae6b67a6801f00c09db24455eee
ilikkako <101609682+ilikkako@users.noreply.github.com>
2025-01-23 15:50:37 +0200

    Add files via upload

---
+++ programming-exercises/problem-sets/ps0001.py
@@ -0,0 +1,6 @@
+yearly_salary = int(input("What is your yearly salary: "))
+portion_saved = int(input("How many percent of your salary you can save: ")) / 100
+cost_of_dream_home = int(input("How much your dream home costs: "))
+portion_down_payment = 0.25
+full_down_payment = cost_of_dream_home * portion_down_payment
+amount_saved = 0
