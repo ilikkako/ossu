ffbd19d60522d1608925469f0c169c2878d86439
ilikkako <101609682+ilikkako@users.noreply.github.com>
2025-01-26 14:20:18 +0200

    Final ps0001.py

--- programming-exercises/problem-sets/ps0001.py
+++ programming-exercises/problem-sets/ps0001.py
@@ -4,3 +4,13 @@ cost_of_dream_home = int(input("How much your dream home costs: "))
 portion_down_payment = 0.25
 full_down_payment = cost_of_dream_home * portion_down_payment
 amount_saved = 0
+return_rate = 0.05 
+months = 0
+
+# Calculate savings monthly 
+while amount_saved < full_down_payment:
+    amount_saved += amount_saved * (return_rate / 12)
+    amount_saved += yearly_salary / 12 * portion_saved 
+    months += 1 
+
+print('Number of months: ' + str(months))
