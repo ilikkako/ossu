0415c1f73de6e9b2c15b23c659af0c614a36c930
ilikkako <101609682+ilikkako@users.noreply.github.com>
2025-03-14 13:27:51 +0200

    Add files via upload

---
+++ finished-programming-exercises/Intro to CS50/others/bisection-search-for-square-root.py
@@ -0,0 +1,18 @@
+# Bisection search for square root
+
+x = 54321
+epsilon = 0.01
+num_guesses = 0
+low = 0
+high = x
+guess = (high + low) / 2
+while abs(guess**2 - x) >= epsilon:
+    if guess**2 > x:
+        high = guess
+    else:
+        low = guess
+    guess = (high + low) / 2
+    num_guesses += 1
+
+print('number of guesses =', num_guesses)
+print(guess, 'is close to square root of', x)
